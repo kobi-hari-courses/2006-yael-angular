81c957bfbad1f746b737582febd6af18413179ac
Kobi Hari <kobi2294@yahoo.com>
2020-06-17 13:35:24 +0300

    Hello Angular example

---
+++ Module 02/hello-angular/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // data
+
+  keyword: string = '';
+  results: string[] = [];
+  isBusy: boolean = false;
+
+  // actions
+  setKeyword(value: string) {
+    this.keyword = value;
+  }
+
+  search() {
+    let currentWord = this.keyword;
+    this.isBusy = true;
+
+    setTimeout(() => {
+      this.results = [
+        currentWord + ' 1', 
+        currentWord + ' 2', 
+        currentWord + ' 3'
+      ];
+      this.isBusy = false;
+    }, 4000);
+
+
+
+  }
+
+
+}
